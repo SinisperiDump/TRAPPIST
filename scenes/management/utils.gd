4a2b3b59500e4fc9a38a6e380760da45dd0c83ee
GildedBirch <thebirchbreaker@gmail.com>
2025-01-18 16:03:58 +0200

    Rays from screen to 3D world

--- scenes/management/utils.gd
+++ scenes/management/utils.gd
@@ -10,15 +10,29 @@ func _ready() -> void:
 	rng.randomize()
 
 
+#region Rays
+# Shoots a ray from screen to 3D world
+func screen_to_world_ray(screen_position: Vector2, mask: int, node_3d: Node3D, collide_with_bodies: bool = true, collide_with_areas: bool = true) -> Dictionary:
+	var space_state = node_3d.get_world_3d().direct_space_state
+	var camera: Camera3D = get_tree().root.get_camera_3d()
+	var ray_origin: Vector3 = camera.project_ray_origin(screen_position)
+	var ray_end: Vector3 = ray_origin + camera.project_ray_normal(screen_position) * 2000
+	var ray = PhysicsRayQueryParameters3D.create(ray_origin, ray_end, mask)
+	ray.collide_with_areas = collide_with_areas
+	ray.collide_with_bodies = collide_with_bodies
+	return space_state.intersect_ray(ray)
+#endregion
+
+
 #region Between checks
 ## Returns true if float value is between given numbers
-func int_between(value: int, min_value: int, max_value:int):
+func int_between(value: int, min_value: int, max_value:int) -> bool:
 	if value >= min_value and value <= max_value:
 		return true
 	return false
 
 ## Returns true if float value is between given numbers
-func float_between(value: float, min_value: float, max_value:float):
+func float_between(value: float, min_value: float, max_value:float) -> bool:
 	if value >= min_value and value <= max_value:
 		return true
 	return false
