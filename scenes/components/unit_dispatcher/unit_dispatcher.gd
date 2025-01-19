965e4bffcb7a5f84b39981196459243379360703
Sinisperi <140267285+Sinisperi@users.noreply.github.com>
2025-01-19 10:54:26 +0200

    created unit dispatcher component

---
+++ scenes/components/unit_dispatcher/unit_dispatcher.gd
@@ -0,0 +1,19 @@
+class_name UnitDispatcher extends Node
+
+signal unit_pushed
+signal unit_popped
+
+var move_order_position: Vector2 = Vector2.ZERO
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	pass
+
+
+func push_unit(unit: Node) -> void:
+	selected_units[unit.get_instance_id()] = unit
+	pass
+
+
+func pop_unit(unit: Node) -> void:
+	selected_units.erase(unit.get_instance_id())
