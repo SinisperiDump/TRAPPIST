680d34e4d707ff9cfbed27a349c333a1d220de9f
Sinisperi <140267285+Sinisperi@users.noreply.github.com>
2025-01-24 16:05:03 +0200

    added non spawnable area to static spawner

--- scenes/aliens/spawner/static_spawner.gd
+++ scenes/aliens/spawner/static_spawner.gd
@@ -3,18 +3,21 @@ extends Node2D
 @export var aliens: Array[PackedScene]
 @export var quantity: int = 0
 @export_range(0.0, 10.0, 0.1) var spawn_rate_ms: float = 1.0
-var spawn_area: CollisionShape2D = null
+@export var spawn_area: CollisionShape2D = null
+@export var not_spawn_area: CollisionShape2D = null
 var radius: float
+var not_spawn_radius: float
 var current_tick: float = 0.0
 var current_quantitiy: int = 0
 
 
 func _ready() -> void:
-	var a = get_child(0)
-	assert(a, "No spawn area has been added to the static spawner " + name)
-	assert(a is CollisionShape2D, "Spawn area is not a CollisionShapet2D node!")
-	spawn_area = a
+	assert(spawn_area, "No spawn area has been added to the static spawner " + name)
+	assert(spawn_area is CollisionShape2D, "Spawn area is not a CollisionShapet2D node!")
+	assert(not_spawn_area, "No not spawn area has been added to the static spawner " + name)
+	assert(not_spawn_area is CollisionShape2D, "Not spawn area is not a CollisionShapet2D node!")
 	radius = spawn_area.get_shape().radius
+	not_spawn_radius = not_spawn_area.get_shape().radius
 
 
 func _physics_process(delta: float) -> void:
@@ -28,7 +31,7 @@ func _physics_process(delta: float) -> void:
 func spawn_alien() -> void:
 	var alien: Alien = AlienManager.create_alien()
 	if alien:
-		var spawn_location = Utils.choose_point_in_rad(self.global_position, radius, radius / 10.0)
+		var spawn_location = Utils.choose_point_in_rad(self.global_position, radius, not_spawn_radius)
 		alien.global_position = spawn_location
 		alien.spawn_type = Alien.SpawnType.NEST
 		alien.init()
